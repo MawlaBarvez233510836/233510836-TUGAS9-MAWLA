29a3cdabd7c5ad3b8386b1f1a04002ae9acba9c1
MawlaBarvez233510836 <mawlabarvez@student.uir.ac.id>
2025-05-11 08:09:31 +0800

    components

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Halo dari Komponen!</h2>
+    <p>Ini adalah komponen Vue sederhana.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h2 {
+  color: teal;
+}
+</style>
